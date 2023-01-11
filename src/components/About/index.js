bc2c537d374d183d77434f1f0134e9515f26387d
Xiaojing Deng <8redfishcreative@gmail.com>
2023-01-11 13:37:37 -0500

    add about me

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <div className='p-5 text-center border-bottom'>
+            <h1>Xiaojing Deng</h1>
+            <div className='col-lg-6 mx-auto'>
+                <p className='lead m-4'>
+                Hi, I'm Xiaojing Deng, Full Stack Web-Developer.
+
+                I am currently taking part in a full Stack Web Development course at the Case Western Reserve University.
+ 
+                </p>
+                <button className='btn btn-primary btn-lg'>contact me</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default About;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 import Navigation from "../../components/Navigation";
-
+import About from "../../components/About";
 class Header extends Component {
   render() {
     return (
         <header>
           <Navigation />
+          <About />
         </header> 
     );
   }
