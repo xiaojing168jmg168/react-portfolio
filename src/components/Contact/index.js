cd054339502dc6f4abdb869988d8d26b06cf18da
Xiaojing Deng <8redfishcreative@gmail.com>
2023-03-07 11:34:40 -0500

    use HeroTofu receive my message for contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
 
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/e8f04950-bcfe-11ed-94a9-db35f1023594";
+
 function Contact() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
@@ -12,54 +14,136 @@ function Contact() {
     function handleChange(e) {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
-    
-                if(!isValid) {
-                    setErrorMessage('please enter a valid email');
-                } else {
-                    setErrorMessage('');
-                }
 
+            if (!isValid) {
+                setErrorMessage('please enter a valid email');
             } else {
-                if (!e.target.value.length) {
-                  setErrorMessage(`${e.target.name} is required.`);
-                } else {
-                  setErrorMessage('');
-                } 
+                setErrorMessage('');
+            }
+
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
         }
 
         if (!errorMessage) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+            setFormState({ ...formState, [e.target.name]: e.target.value })
         }
     }
+    const [status, setStatus] = useState();
 
-    function handleSubmit(e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
+
+        // Anything you need to inject dynamically
+        const injectedData = {
+            DYNAMIC_DATA_EXAMPLE: 123,
+        };
+        const inputs = e.target.elements;
+        const data = {};
+
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs[i].name) {
+                data[inputs[i].name] = inputs[i].value;
+            }
+        }
+
+        Object.assign(data, injectedData);
+
+        fetch(FORM_ENDPOINT, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => {
+                // It's likely a spam/bot request, so bypass it to validate via captcha
+                if (response.status === 422) {
+                    Object.keys(injectedData).forEach((key) => {
+                        const el = document.createElement("input");
+                        el.type = "hidden";
+                        el.name = key;
+                        el.value = injectedData[key];
+
+                        e.target.appendChild(el);
+                    });
+
+                    e.target.submit();
+                    throw new Error("Please finish the captcha challenge");
+                }
+
+                if (response.status !== 200) {
+                    throw new Error(response.statusText);
+                }
+
+                return response.json();
+            })
+            .then(() => setStatus("We'll be in touch soon."))
+            .catch((err) => setStatus(err.toString()));
+    };
+
+    if (status) {
+        return (
+            <>
+                <div className="text-2xl">Thank you!</div>
+                <div className="text-md">{status}</div>
+            </>
+        );
     }
+
     return (
         <div className='container container-small'>
             <h2 className='mt-5 mb-5'>Contact Me</h2>
-            <div className="mb-3">
-                <label for="exampleFormControlInput1" className="form-label">Name</label>
-                <input type="text" name="name" defaultValue={name} onBlur={handleChange} className="form-control" id="exampleFormControlInput1" placeholder=""/>
-            </div>
-            <div className="mb-3">
-                <label for="exampleFormControlInput1" className="form-label">Email address</label>
-                <input type="email" name="email" defaultValue={email} onBlur={handleChange} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-            </div>
-            <div className="mb-3">
-                <label for="exampleFormControlTextarea1" className="form-label">Message</label>
-                <br></br>
-                <textarea className="textarea" name="message" defaultValue={message} onBlur={handleChange} id="exampleFormControlTextarea1" rows="5"></textarea>
-            </div>
-            {errorMessage && (
-                <div>
-                    <p className="is-danger">{errorMessage}</p>
+           
+            <form
+                action={FORM_ENDPOINT}
+                onSubmit={handleSubmit}
+                method="POST"
+                target="_blank"
+            >
+                <div className="mb-3 pt-0">
+                    <input
+                        type="text"
+                        placeholder="Your name"
+                        onBlur={handleChange}
+                        name="name"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                        required
+                    />
                 </div>
-            )}
-            <div class="mb-5" >
-                <button data-testid='button' class="btn btn-primary btn-lg " type="submit" onSubmit={handleSubmit}>Submit</button>
-            </div>
-
+                <div className="mb-3 pt-0">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        onBlur={handleChange}
+                        name="email"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                        required
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <textarea
+                        placeholder="Your message"
+                        onBlur={handleChange}
+                        name="message"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                        required
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <button
+                        className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="submit"
+                    >
+                        Send a message
+                    </button>
+                </div>
+            </form>
         </div>
     );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,18 @@ import About from "../About";
 import Contact from "../Contact";
 import Portfolio from "../Portfolio";
 import Resume from "../Resume";
-
+import { useEffect } from "react";
 function Header() {
   const [currentPage, setCurrentPage] = useState("About");
+  useEffect(() => {
+    if (document) {
+      const stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.href = "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
 
+      document.head.appendChild(stylesheet);
+    }
+  }, []);
   // The renderPage method uses a switch statement to render the appropriate current page
   const renderPage = () => {
     switch (currentPage) {
